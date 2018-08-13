cb51dad4ea74172b053061ae14d88d83567bce56
Wei-Zhou, Hong <jhon800703@gmail.com>
2018-08-13 17:23:05 +0800

    Updata 2.9.2
    BGM LIST, Random rework

--- MenuBGMLabel.cs
+++ MenuBGMLabel.cs
@@ -2,10 +2,10 @@ using System;
 
 namespace YgomGame.Utility
 {
-	// Token: 0x02000546 RID: 1350
+	// Token: 0x02000582 RID: 1410
 	public partial class SoundUtil
 	{
-		// Token: 0x06003054 RID: 12372 RVA: 0x0014BDBC File Offset: 0x0014A1BC
+		// Token: 0x06003421 RID: 13345
 		public static string MenuBGMLabel()
 		{
 			Random rnd = new Random();
@@ -22,17 +22,8 @@ namespace YgomGame.Utility
 				"BGM_IJGN_MENU"
 			};
 			int bgm_num = rnd.Next(0, bgm.Length);
-			string result = "BGM_MENU_MAIN";
 			string currentSeries = CharaUtil.currentSeries;
-			result = bgm[bgm_num];
-			if (currentSeries != null)
-			{
-				if (currentSeries == "GX")
-				{
-					result = bgm[bgm_num];
-				}
-			}
-			return result;
+			return bgm[bgm_num];
 		}
 	}
 }
