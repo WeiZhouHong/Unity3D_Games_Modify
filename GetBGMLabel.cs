15c0d6c373963599d52a39b7232a669421ee99c7
Wei-Zhou, Hong <jhon800703@gmail.com>
2018-08-13 17:20:36 +0800

    Updata 2.9.2
    BGM LIST

--- GetBGMLabel.cs
+++ GetBGMLabel.cs
@@ -5,15 +5,18 @@ using System.Diagnostics;
 
 namespace YgomGame.Duel
 {
-	// Token: 0x0200016C RID: 364
+	// Token: 0x0200010F RID: 271
 	public partial class Sound
 	{
-		// Token: 0x06000D45 RID: 3397 RVA: 0x00068500 File Offset: 0x00066700
+		// Token: 0x06000CEB RID: 3307
 		public static string GetBGMLabel()
 		{
 			Random rnd = new Random();
 			string[] bgm = new string[]
 			{
+				"BGM_DUEL_01a",
+				"BGM_DUEL_02",
+				"BGM_DUEL_03",
 				"BGM_DUEL_04",
 				"BGM_DUEL_05",
 				"BGM_DUEL_06",
@@ -21,19 +24,23 @@ namespace YgomGame.Duel
 				"BGM_DUEL_08",
 				"BGM_DUEL_09",
 				"BGM_DUEL_10",
+				"BGM_DUEL_11",
+				"BGM_DUEL_12",
 				"BGM_DUEL_13",
+				"BGM_DUEL_14",
+				"BGM_DUEL_16",
+				"BGM_DUEL_17",
+				"BGM_DUEL_18",
 				"BGM_DUEL_19",
-				"BGM_DUEL_TAG",
-				"BGM_EV_ISLAND_MAIN"
+				"BGM_DUEL_20",
+				"BGM_DUEL_TAG"
 			};
-	
 			if (Util.GetTutorialNO() == 1)
 			{
 				return null;
 			}
 			int bgm_num = rnd.Next(0, bgm.Length);
-			string result = "BGM_DUEL_TAG";
-			result = bgm[bgm_num];
+			string result = bgm[bgm_num];
 			if (Sound.s_work != null)
 			{
 				result = bgm[bgm_num];
